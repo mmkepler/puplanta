2e2f12fe2784e30e87f8f299c66034dd1918f0bd
Melissa Kepler <mmkepler@yahoo.com>
2019-03-03 15:48:31 -0500

    updated README

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ###  A website set up to prove my ability to serve it from AWS
 
 ####  This is a WIP, check back for updates
+####  I am building an api to deal with the store locations. This is such a large amount of data that it would take several requests sent to the google places/maps api to complete the page. I would have to pay for all those requests. Since I could not find an existing api that had all the pet stores in the ~60 mi radius of Atlanta, I am gathering data to create one. This means this project will take a little longer, but then if I wanted to turn this project into a "real" site I will have the capablility.
 
 ###  Technology Used:
 *  HTML
 *  CSS
 *  JavaScript/React
 *  Google Maps API
-*  Google Places
 *  Amazon S3/Route 53
--- src/components/Stores.js
+++ src/components/Stores.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import Map from './Map.js';
-//import axios from 'axios';
+
 
 export default class Stores extends Component {
   constructor(props) {
     super(props);
+    this.state = {data: this.props};
   }
 
   componentWillMount() {
-    let locations;
-  /*axios.get('https://cors-anywhere.herokuapp.com/https://api.petsmart.com/v1/search/stores?model.requestMetric=false&model.latitude=33.8884544&model.longitude=-84.3842454')
-    .then(response => {
-      
-      console.log(response.data);
-    })
-    .catch(err => {
-      console.log("error " + err);
-    });*/
+  
     
   }
 
@@ -25,7 +17,6 @@ export default class Stores extends Component {
     return (
       <div>
         <h1>Stores coming soon!</h1>
-      
       </div>
     )
   }
